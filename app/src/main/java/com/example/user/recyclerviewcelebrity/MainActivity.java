60b520f28602a3556a51306968bf5eba68c600c0
joseph alemu <josephalemu01@gmail.com>
2018-01-19 06:07:34 -0500

    first commit

---
+++ app/src/main/java/com/example/user/recyclerviewcelebrity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.user.recyclerviewcelebrity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String [] name= {
+
+                "User1",
+                "User2",
+                "User3",
+                "User4",
+                "User5",
+                "User6",
+                "User7",
+                "User8",
+                "User9",
+                "User10",
+                "User11",
+
+
+
+
+        };
+
+    }
+}
---
+++ app/src/main/java/com/example/user/recyclerviewcelebrity/RecyclerAdapter.java
@@ -0,0 +1,8 @@
+package com.example.user.recyclerviewcelebrity;
+
+/**
+ * Created by user on 1/19/2018.
+ */
+
+public class RecyclerAdapter {
+}
